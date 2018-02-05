2b1aff5d62df82f05fd473a8111ef0d753c4235f
bijalmajethia <35801945+bijalmajethia@users.noreply.github.com>
2018-02-05 10:21:27 +0530

    Delete DuplicateNameException.java

--- mod 3 mpt/RememberMe/src/com/capgemini/contacts/exception/DuplicateNameException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.contacts.exception;
-
-public class DuplicateNameException extends Exception {
-	public DuplicateNameException(){
-		System.out.println("Duplicate Contact. Failed to add the Contact");
-	}
-}
